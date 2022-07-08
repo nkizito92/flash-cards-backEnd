c350ad59a2be5bda522a3f96043c9581651693b3
Kizito Njoku <nkizito57@gmail.com>
2022-07-07 23:07:34 -0500

    clean up and refactored lines of code

--- backEndCard/App/views.py
+++ backEndCard/App/views.py
@@ -1,55 +1,26 @@
 import pdb
-from telnetlib import WONT
-import weakref
 from django.shortcuts import render
-from django.http import Http404
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from django.http import JsonResponse
-from django.core import serializers
-from .serializers import WordSerializer
-from django.conf import settings
-from .models import Word
-import json
+from .utils import showWordsList, updateWord, showWordDetail, deleteWord, createWord
 
 # Create your views here.
-@api_view(['GET'])
-def getData(request):
-    peps = Word.objects.all()
-    serializer = WordSerializer(peps, many=True)
-    return Response(serializer.data)
 
-@api_view(['GET'])
-def getWord(request, primaryKey):
-    
-    thisWord = Word.objects.get(id=primaryKey)
-    serializer = WordSerializer(thisWord, many=False)
-    return Response(serializer.data)
-# Create a Word
-@api_view(['POST'])
-def addWord(request):
-    newWord = request.data
-    word = Word.objects.create(
-        name=newWord['word']['name'], definition=newWord['word']['definition']
-    )
-    serializer = WordSerializer(word, many=False)
-    # To debug first import pdb then from desired line type pdb.set_trace()
-    return Response(serializer.data)
-#Update View
-@api_view(['PATCH'])
-def updateWord(request, pk):
-    data = request.data['word']
-    # To get the specific object use objects.get(id=pk) "pk is Primary Key"
-    word = Word.objects.get(id=pk)
-    serializer = WordSerializer(instance=word, data=data)
-    if serializer.is_valid():
-      serializer.save() 
-    return Response(serializer.data)
-
-@api_view(["DELETE"])
-def deleteWord(request, pk):
-    word = Word.objects.get(id=pk)
-    word.delete()
-    return Response("Delete {word.name}")
+@api_view(['GET','POST'])    
+def wordRequest(request):
+    if(request.method == 'GET'):
+     return showWordsList(request)
+    # Post Request
+    if(request.method == 'POST'):
+     return createWord(request)
+   
+@api_view(['GET','PATCH','DELETE'])
+def wordChangeRequest(request, pk):
+    # Show Word Details
+     if(request.method == 'GET'):
+        return showWordDetail(request, pk)
+    # Update Request
+     if(request.method == 'PATCH'):
+       return updateWord(request, pk)
+    # Delete request
+     if(request.method == 'DELETE'):
+        return deleteWord(request, pk)
