28df31ecb7191e2281065d747a00ebaa2a968149
Kizito Njoku <nkizito57@gmail.com>
2022-07-11 23:00:38 -0500

    added the attribute user to the word class model

--- backEndCard/App/models.py
+++ backEndCard/App/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-
+# import what is below to add a user
+from django.contrib.auth.models import User
 # Create your models here.
+
+
 class Word(models.Model):
+    # setting user for authenticate 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200)
     updated = models.DateTimeField(auto_now=True)
     definition = models.TextField(max_length=1000)
@@ -9,3 +14,6 @@ class Word(models.Model):
 
     def __str__(self):
         return self.name[0:50]
+
+
+
