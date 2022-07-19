049d5625663a15a75ce366ce44e63d349f37388a
Kizito Njoku <nkizito57@gmail.com>
2022-07-18 22:52:54 -0500

    added permission_classes module

--- backEndCard/App/views.py
+++ backEndCard/App/views.py
@@ -1,11 +1,11 @@
 import pdb
 from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from .utils import showWordsList, updateWord, showWordDetail, deleteWord, createWord
-
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
 # how to customize your token claims
 # use jwt.io to view json data about from the access token
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -30,13 +30,22 @@ def getRoutes(request):
     ]
     return Response(routes)
 
-@api_view(['GET','POST'])    
+# adding permissions for Authenticating
+@api_view(['GET','POST'])   
+@permission_classes([IsAuthenticated])
 def wordRequest(request):
+    # grabbing the user's input
     if(request.method == 'GET'):
      return showWordsList(request)
     # Post Request
     if(request.method == 'POST'):
      return createWord(request)
+
+@api_view(['POST'])
+def creatingUser(request):
+    if(request.method == "POST"):
+        return createUser(request)
+
    
 @api_view(['GET','PATCH','DELETE'])
 def wordChangeRequest(request, pk):
