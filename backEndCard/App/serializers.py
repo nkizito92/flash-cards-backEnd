ff0d97bcc69a02a816054b46298ae4051c9f51d9
Kizito Njoku <nkizito57@gmail.com>
2022-08-01 20:06:08 -0500

    serializers created for serializing models

--- backEndCard/App/serializers.py
+++ backEndCard/App/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from App.models import Word
 # You need this serializer to serialize the objects created from models
+# Also to create json objects out of python
 class WordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Word
         fields = '__all__'
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = '__all__'
